a68a2acdb3aaf8261fcfecb93d15e7fac6737475
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-05 08:04:47 +0530

    advanced for loop for using for loop easily

---
+++ Enhanced_for_loop.java
@@ -0,0 +1,15 @@
+package Training;
+
+public class Enhanced_for_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numbers[]= {10,20,30,40,50,60,70,80};
+		for (int n: numbers)
+		{
+			System.out.println(n);
+		}
+
+	}
+
+}
