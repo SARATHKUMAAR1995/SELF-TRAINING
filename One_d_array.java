ad1f82f9ae2b1138d1014b4a951230a91d095a5c
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-07 16:24:05 +0530

    To print an one dimensional array using enhanced for loop

---
+++ One_d_array.java
@@ -0,0 +1,22 @@
+package Training;
+
+public class One_d_array {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,50,60,70,80,90,100};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+System.out.println("--------------------------------------");
+// enhanced for loop
+for(int element :a)
+{
+	System.out.println(element);
+}
+int b[]; // declaring array
+b = new int[10]; // allocating memory
+// (data type)( varaible name) =new (data type[memory size])   = syntax
+	}
+
+}
