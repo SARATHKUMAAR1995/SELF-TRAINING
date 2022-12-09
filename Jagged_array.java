a7ceb9a8ed0006d955e1a7cd3a2c1cdf9439f9f7
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-09 08:06:38 +0530

    To print 2d array

---
+++ Jagged_array.java
@@ -0,0 +1,21 @@
+package Training;
+
+public class Jagged_array {
+
+	public static void main(String[] args) {
+		int a[][]= {
+				{10,20,30,40},
+				{10,20,30},
+				{10,20,30,50}
+		};
+		for(int i = 0;i<a.length;i++) {
+			for(int j =0;j<a[i].length;j++)
+			{
+				System.out.print(" "+a[i][j]);
+			}
+			System.out.println("");	
+		}
+
+	}
+
+}
