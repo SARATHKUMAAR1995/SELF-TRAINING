8e661cc04f5571609e1a8efa60b3961acd62a0fb
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-10 11:27:19 +0530

    Concepts involved in String Buffer

---
+++ String_buffer.java
@@ -0,0 +1,48 @@
+package Training;
+
+public class String_buffer {
+
+	public static void main(String[] args) {
+		StringBuffer a = new StringBuffer("Tutor");
+		System.out.println(a);
+
+		a.append(" Joes");
+		System.out.println(a);
+
+		a.insert(10, " Computer");
+		System.out.println(a);
+
+		a.replace(9, 11, "@@@");
+		System.out.println(a);
+
+		a.delete(9, 11);
+		System.out.println(a);
+
+		a.reverse();
+		System.out.println(a);
+
+		// to find the character at the specified position
+		System.out.println(a.charAt(2));
+
+		// to find length
+		System.out.println(a.length());
+
+		// to find the values using index
+		System.out.println(a.substring(0));
+		System.out.println(a.substring(0, 5));
+
+		// to set change the index position
+		a.setCharAt(0, '@');
+		System.out.println(a);
+
+		// to find the memory size use capacity keyword
+		StringBuffer sb = new StringBuffer();
+		System.out.println(sb.capacity());// default 16
+		sb.append("Hello");
+		System.out.println(sb.capacity());// now 16
+		sb.append("java is my favourite language");
+		System.out.println(sb.capacity());
+
+	}
+
+}
