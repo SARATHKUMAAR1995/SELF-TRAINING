8d213e35054cba362ef280a7a7b21ad3067cb20f
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-10 11:32:51 +0530

    To count uppercase, lowercase, space, number, vovels & symbols in a string

---
+++ Count_character.java
@@ -0,0 +1,39 @@
+//to count uppercase, lowercase, space, number, vovels & symbols in a string
+package Training;
+
+public class Count_character {
+
+	public static void main(String[] args) {
+		StringBuilder a = new StringBuilder("Sarath-age is 27@");
+		System.out.println(a);
+		int upper = 0, lower = 0, space = 0, number = 0, vowels = 0;
+		for (int i = 0; i < a.length(); i++) {
+			// using unicode or ASCII value
+			if (a.charAt(i) >= 97 && a.charAt(i) <= 122) {
+				lower++;
+			}
+			if (a.charAt(i) >= 65 && a.charAt(i) <= 90) {
+				upper++;
+			}
+			if (a.charAt(i) == 32) {
+				space++;
+			}
+			if (a.charAt(i) >= 48 && a.charAt(i) <= 57) {
+				number++;
+			}
+			if (a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o'
+					|| a.charAt(i) == 'u' || a.charAt(i) == 'A' || a.charAt(i) == 'E' || a.charAt(i) == 'I'
+					|| a.charAt(i) == 'O' || a.charAt(i) == 'U') {
+				vowels++;
+			}
+		}
+		System.out.println("Uppercase : " + upper);
+		System.out.println("Lowercase : " + lower);
+		System.out.println("Space     : " + space);
+		System.out.println("Number    : " + number);
+		System.out.println("vowels    : " + vowels);
+		System.out.println("Symbols   : " + (a.length() - (upper + lower + space + number)));
+
+	}
+
+}
