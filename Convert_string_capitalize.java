3f56f99d033c936120fa07b4a492c9eda7ced6b7
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-11 21:58:12 +0530

    To converty a String to Capitalize

---
+++ Convert_string_capitalize.java
@@ -0,0 +1,29 @@
+package Training;
+
+public class Convert_string_capitalize {
+	public static void main(String args[]) {
+		StringBuilder a = new StringBuilder("saRath kuMaar traiNing");
+		System.out.println("Original string      : " + a);
+		// first letter has to be manually checked & changed
+		if (a.charAt(0) >= 97 && a.charAt(0) <= 122) {
+			int c = (int) a.charAt(0) - 32;
+			a.setCharAt(0, (char) c);
+		}
+		for (int i = 1; i < a.length(); i++) {
+			if (a.charAt(i) == 32) {
+				i++;
+				if (a.charAt(i) >= 97 && a.charAt(i) <= 122) {
+					int c = (int) a.charAt(i) - 32;
+					a.setCharAt(i, (char) c);
+				}
+			} else {
+				if (a.charAt(i) >= 65 && a.charAt(i) <= 90) {
+					int c = (int) a.charAt(i) + 32;
+					a.setCharAt(i, (char) c);
+				}
+			}
+		}
+		System.out.println("Capitalized string   : " + a);
+
+	}
+}
