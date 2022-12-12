46ddc45bf6893ae983ca167df4bd872e15d9a61b
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-12 05:48:18 +0530

    Using built in math function

---
+++ Math_functions.java
@@ -0,0 +1,25 @@
+// Built in math functions
+package Training;
+
+public class Math_functions {
+
+	public static void main(String[] args) {
+
+		System.out.println("Absolute value    : " + Math.abs(-45));
+		System.out.println("Round vaslue      : " + Math.round(2.288));
+		System.out.println("Ceil value        : " + Math.ceil(2.588));
+		System.out.println("Floor value       : " + Math.floor(2.588));
+
+		double a = 2;
+		double b = 3;
+		System.out.println("Maximum number    : " + Math.max(a, b));
+		System.out.println("Square root value : " + Math.sqrt(b));
+		System.out.println("Power of a & b is : " + Math.pow(a, b));
+		System.out.println("Logarithm of a is : " + Math.log(a));
+		System.out.println("Sin value of a is : " + Math.sin(a));
+		System.out.println("Cos value of a is : " + Math.cos(a));
+		System.out.println("Tan value of a is : " + Math.tan(a));
+
+	}
+
+}
