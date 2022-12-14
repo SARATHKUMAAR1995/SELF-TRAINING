0d9780d2efd99cf6065a10feb3098e7fb44bc8b8
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-14 21:39:11 +0530

    Different types of object creation & usage

---
+++ Class_object.java
@@ -0,0 +1,30 @@
+package Training;
+
+class Rectangle {
+
+	int length, width;
+
+	void getDetails(int x, int y) {
+		length = x;
+		width = y;
+	}
+
+	int area() {
+		int a = length * width;
+		return a;
+	}
+}
+
+public class Class_object {
+	public static void main(String args[]) {
+		Rectangle o1 = new Rectangle();
+		o1.length = 10;
+		o1.width = 20;
+		System.out.println("Area of Rectangle : " + o1.area());
+
+		Rectangle o2 = new Rectangle();
+		o2.getDetails(20, 30);
+		System.out.println("Area of Rectangle : " + o2.area());
+
+	}
+}
