62c6b82f1bcf02a52acc57dab40d9de1680712ce
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-16 08:07:14 +0530

    Sample program for hierarchical inheritance

---
+++ Inheritance_hierarchical.java
@@ -0,0 +1,54 @@
+package Training;
+
+// Hierarchical inheritance
+class shape1 {
+	float length, breadth, radius;
+}
+
+class rect extends shape1 {
+	public rect(float l, float b) {
+		length = l;
+		breadth = b;
+	}
+
+	float rectangle_area() {
+		return length * breadth;
+	}
+}
+
+class circle extends shape1 {
+	public circle(float r) {
+		radius = r;
+	}
+
+	float circle_area() {
+		return 3.14f * (radius * radius);
+	}
+}
+
+class square extends shape1 {
+	public square(float l) {
+		length = l;
+	}
+
+	float square_area() {
+		return (length * length);
+	}
+}
+
+public class Inheritance_hierarchical {
+
+	public static void main(String[] args) {
+
+		rect o1 = new rect(2, 5);
+		System.out.println("Area of Rectangle : " + o1.rectangle_area());
+
+		circle o2 = new circle(5);
+		System.out.println("Area of Circle : " + o2.circle_area());
+
+		square o3 = new square(3);
+		System.out.println("Area of Square : " + o3.square_area());
+
+	}
+
+}
