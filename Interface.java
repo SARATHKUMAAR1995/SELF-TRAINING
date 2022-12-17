5bc9c107c1588754d675f9bb0838120de42b6545
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-17 21:33:32 +0530

    Example of Interface

---
+++ Interface.java
@@ -0,0 +1,30 @@
+package Training;
+
+interface Animal {
+	void Sound();
+
+	void sleep();
+}
+
+class Dog implements Animal {
+	@Override
+	public void Sound() {
+		System.out.println("The Dog Sounds like : woof");
+	}
+
+	@Override
+	public void sleep() {
+		System.out.println("Dog Sleeping");
+	}
+}
+
+public class Interface {
+
+	public static void main(String[] args) {
+		Dog o = new Dog();
+		o.Sound();
+		o.sleep();
+
+	}
+
+}
