bd50845abcf41c10a4d57839ff6b395cd89287eb
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-20 18:18:54 +0530

    Static belongs to a class not for object

---
+++ Static_var_method.java
@@ -0,0 +1,32 @@
+// Static belongs to a class not for object
+package Training;
+
+class staticTest
+{
+    static int a=10;
+    int b=20;
+    void show()
+    {
+        System.out.println("A : "+a+" B : "+b);
+    }
+    static void display()
+    {
+        System.out.println("A : "+a);
+    }
+}
+
+public class Static_var_method {
+
+	public static void main(String[] args) {
+		
+		staticTest o1=new staticTest();
+        o1.show();
+        staticTest o2=new staticTest();
+        o2.b=100;
+        staticTest.a=200;
+        o2.show();
+        o1.show();
+
+	}
+
+}
